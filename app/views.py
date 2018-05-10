9513c43ee29f73aecf048421c7d80ab00b607fe5
River Gilhuly <rivergilhuly@gmail.com>
2018-05-10 08:59:55 -0400

    Hid non-game pages

--- app/views.py
+++ app/views.py
@@ -2,18 +2,19 @@ from flask import render_template
 
 from app import app
 
+# @app.route('/')
+# def index():
+# 	return render_template("index.html")
+
+# @app.route('/about')
+# def about():
+# 	return render_template("about.html")
+
+# Just serve the game at the index
 @app.route('/')
-def index():
-	return render_template("index.html")
-
-@app.route('/about')
-def about():
-	return render_template("about.html")
-
-@app.route('/game')
 def game():
 	return render_template("game.html")
 
-@app.route('/chat')
-def chat():
-	return render_template("chat.html")
+# @app.route('/chat')
+# def chat():
+# 	return render_template("chat.html")
