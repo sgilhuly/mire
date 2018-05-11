5d780442f9df6709a21255a55c4c760ecd564629
Sean Gilhuly <gilhulysean@gmail.com>
2018-05-11 11:36:15 -0400

    Added simple chat functionality

--- app/connection.py
+++ app/connection.py
@@ -1,24 +1,37 @@
 from flask import render_template, request
+from flask_socketio import join_room, leave_room, send
 
-from app import socketio
+from app import app, socketio
 
-# @socketio.on('message')
-# def handle_message(message):
-# 	print('broadcasting message: ' + message)
-# 	socketio.send(message, broadcast=True)
+class Connection():
+	def __init__(self):
+		self.player = None
 
-# @socketio.on('json')
-# def handle_json(json):
-# 	print('received json: ' + str(json))
-
-# @socketio.on('my event')
-# def handle_my_custom_event(json):
-# 	print('received custom event json: ' + str(json))
+def get_connection():
+	return app.game.active_connections[request.sid]
 
 @socketio.on('connect')
 def handle_connect():
-	socketio.send('A cloaked figure emerges from the mist.\nHe beckons you closer, whispering:\n"Welcome to MIRE, traveller. What is your name?"')
+	send('A cloaked figure emerges from the mist.\nHe beckons you closer, whispering:\n\n"Welcome to MIRE, traveller. What is your name?"')
+	app.game.active_connections[request.sid] = Connection()
 
 @socketio.on('message')
 def handle_message(message):
-	socketio.send(message)
+	# Maybe the connection was ignored due to inactivity
+	try:
+		conn = get_connection()
+	except KeyError:
+		send('\nYour connection is invalid, please refresh the page.')
+		return
+
+	send('> %s' % message)
+
+	# Login, or create a player
+	if not conn.player:
+		conn.player = message
+		send('Okay, you are named "%s".' % conn.player)
+		join_room('chat')
+		return
+
+	# Decide what the player's action was
+	send('%s: %s' % (conn.player, message), room='chat', broadcast=True)
