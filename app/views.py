88333136bdac1f0e4608496b0ab18d49e5bfe912
Sean Gilhuly <gilhulysean@gmail.com>
2018-05-10 21:31:24 -0400

    Created game html page

--- app/views.py
+++ app/views.py
@@ -4,17 +4,17 @@ from app import app
 
 # @app.route('/')
 # def index():
-# 	return render_template("index.html")
+# 	return render_template('index.html')
 
 # @app.route('/about')
 # def about():
-# 	return render_template("about.html")
+# 	return render_template('about.html')
 
 # Just serve the game at the index
 @app.route('/')
 def game():
-	return render_template("game.html")
+	return render_template('game.html')
 
 # @app.route('/chat')
 # def chat():
-# 	return render_template("chat.html")
+# 	return render_template('chat.html')
