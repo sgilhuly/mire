04b1e1c503f06b6a0b58d4c47041ecc8d7402d21
River Gilhuly <rivergilhuly@gmail.com>
2018-05-14 22:48:17 -0400

    Added more character creation stuff, including classes (that don't do anything yet)

--- app/connection.py
+++ app/connection.py
@@ -23,6 +23,7 @@ def handle_disconnect():
 
 @socketio.on('message')
 def handle_message(message):
+	message = message.strip()
 	print('%s: %s' % (request.sid, message))
 	# Maybe the connection was ignored due to inactivity
 	try:
@@ -31,18 +32,54 @@ def handle_message(message):
 		send('\nYour connection is invalid, please refresh the page.')
 		return
 
-	send('> %s' % message)
+	send('\n> %s\n' % message)
 
-	# Login, or create a player
+	# Check if character details need to be filled out
 	if not conn.player:
+		if not 4 <= len(message) <= 20:
+			send('The figure acts as if he didn\'t hear you right, and gestures as if to say:\n"Choose a name between 4 and 20 characters long."')
+			return
 		conn.player = Player(message, app.game.random_room())
-		send('Okay, you are named "%s".' % conn.player.name)
-		join_room('chat')
+		send('"Welcome, %s. Before you lies a great maze.\nYou connection to this world is tenuous, and you will have only 100 steps to\nreach the goal. You may need to rely on others to complete your jounery.\n\nNow, may I ask what your profession is?"' % conn.player.name)
+		send('\n(Type 1 - 7 to select one of the following)\n  1 - SCOUTER\n  2 - SENSER\n  3 - SIGNALLER\n  4 - SIGNER\n  5 - SIMPLETON\n  6 - SNIFFER\n  7 - SOUNDER')
 		return
 
-	# Decide what the player's action was
 	args = message.split(' ')
 
+	if not conn.player.type_confirmed:
+		if conn.player.type == Player.TYPE_NONE:
+			if args[0] == '1':
+				conn.player.type = Player.TYPE_SCOUTER
+			if args[0] == '2':
+				conn.player.type = Player.TYPE_SENSER
+			if args[0] == '3':
+				conn.player.type = Player.TYPE_SIGNALLER
+			if args[0] == '4':
+				conn.player.type = Player.TYPE_SIGNER
+			if args[0] == '5':
+				conn.player.type = Player.TYPE_SIMPLETON
+			if args[0] == '6':
+				conn.player.type = Player.TYPE_SNIFFER
+			if args[0] == '7':
+				conn.player.type = Player.TYPE_SOUNDER
+			send(conn.player.describe_class())
+			if conn.player.type != Player.TYPE_NONE:
+				send('\n"Is this an accurate description of yourself?" (yes/no)')
+			return
+		else:
+			if args[0] == 'yes' or args[0] == 'y':
+				conn.player.type_confirmed = True
+				send('The figure bows. "Your goal lies near the south east corner of the maze."\n\nYou feel the sensation of jolting awake, though your eyes were already open.\n(Type "help" for general commands, or "help class" for class details.)')
+				return
+			elif args[0] == 'no' or args[0] == 'n':
+				conn.player.type = Player.TYPE_NONE
+				send('(Type 1 - 7 to select one of the following)\n  1 - SCOUTER\n  2 - SENSER\n  3 - SIGNALLER\n  4 - SIGNER\n  5 - SIMPLETON\n  6 - SNIFFER\n  7 - SOUNDER')
+				return
+			else:
+				send('"Well, yes or no?"')
+				return
+
+	# Decide what the player's action was
 	if not args:
 		send('You pause for a moment and think.')
 
--- app/player.py
+++ app/player.py
@@ -1,7 +1,56 @@
 from app import app
 from app.room import Room
 
+# Character classes
+# Signaller: knows direction to exit (has a compass)
+# Sounder: knows distance to wall, if direction leads to dead end (echoes)
+# Scouter: has higher move limit, speaks in all caps
+# Senser: knows grid coordinates, blind
+# Sniffer: knows logarithmic distance to exit (smell strength), can't speak (is a dog)
+# Signer: can leave a few messages
+# Simpleton: No abilities
+
 class Player():
+	TYPE_NONE = 0
+	TYPE_SCOUTER = 1
+	TYPE_SENSER = 2
+	TYPE_SIGNALLER = 3
+	TYPE_SIGNER = 4
+	TYPE_SIMPLETON = 5
+	TYPE_SNIFFER = 6
+	TYPE_SOUNDER = 7
+
+	TYPE_NAMES = {
+		1: 'SCOUTER',
+		2: 'SENSER',
+		3: 'SIGNALLER',
+		4: 'SIGNER',
+		5: 'SIMPLETON',
+		6: 'SNIFFER',
+		7: 'SOUNDER'
+	}
+
 	def __init__(self, name, room):
 		self.name = name
 		self.room = room
+		self.type = Player.TYPE_NONE
+		self.type_confirmed = False
+		self.steps_left = 100
+
+	def describe_class(self):
+		if self.type == Player.TYPE_SCOUTER:
+			return 'SCOUTER\n\nFaster and more energetic than the others. A scouter can move 200 spaces\ninstead of 100. Others may find this constant energy annoying.'
+		elif self.type == Player.TYPE_SENSER:
+			return 'SENSER\n\nIn tune with an innate location sense. A senser always knows their exact grid\nlocation. This sense makes up for a permanent blindness.'
+		elif self.type == Player.TYPE_SIGNALLER:
+			return 'SIGNALLER\n\nJust a person with a compass.'
+		elif self.type == Player.TYPE_SIGNER:
+			return 'SIGNER\n\nPosesses a magical marking skill. A signer can leave up to 5 magic signs, each\nwith a short message.'
+		elif self.type == Player.TYPE_SIMPLETON:
+			return 'SIMPLETON\n\nJust a person.'
+		elif self.type == Player.TYPE_SNIFFER:
+			return 'SNIFFER\n\nA dog. A sniffer has a powerful sense of smell that can tell when the exit is\nclose. Can be heard from further away by barking, but can not speak.'
+		elif self.type == Player.TYPE_SOUNDER:
+			return 'SOUNDER\n\nHas a fine sense of hearing. A sounder can sing in any direction, and tell how\nfar away the nearest wall is. Can also find dead ends by listening for echoes.'
+		else:
+			return '(Type 1 - 7 to select a class)'
