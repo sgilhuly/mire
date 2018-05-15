b6d0a8c26f74b56910e51699f2b5daa4ee554f42
Sean Gilhuly <gilhulysean@gmail.com>
2018-05-15 13:10:05 -0400

    Added command line option for server port

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
+import sys
+
 from app import app, socketio
 
 if __name__ == "__main__":
-	socketio.run(app, host="0.0.0.0", port=8000)
+	if len(sys.argv) > 1:
+		port = int(sys.argv[1])
+	else:
+		port=5000
+	socketio.run(app, host="0.0.0.0", port=port)
